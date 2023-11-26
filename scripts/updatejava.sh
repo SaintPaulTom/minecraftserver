988d86bfea579f54abaa8162bbaac78078dc3778
SaintPaulTom <151305081+SaintPaulTom@users.noreply.github.com>
2023-11-26 13:11:57 -0600

    Update updatejava.sh
    change echo pipes to standard pipes.
    changed download command to show progress

--- scripts/updatejava.sh
+++ scripts/updatejava.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-NORMAL_OUT=/dev/stdout
-ERROR_OUT=/dev/stderr
 VERSION_MANIFEST='https://piston-meta.mojang.com/mc/game/version_manifest.json'
 JAR_PATH=/opt/minecraft/java/jars
 NORMAL_OUT=/dev/stdout
@@ -13,56 +11,56 @@ VERSION_RELEASE=$(echo "$VERSION_MANIFEST_DATA" | jq -r '.latest.release' )
 VERSION_SNAPSHOT=$(echo "$VERSION_MANIFEST_DATA" | jq -r '.latest.snapshot' )
 RELEASE_JAR=$JAR_PATH/minecraft_server.$VERSION_RELEASE.jar
 SNAPSHOT_JAR=$JAR_PATH/minecraft_server.$VERSION_SNAPSHOT.jar
-echo "Done" > $NORMAL_OUT
+echo "Done" >&1
 
-echo "Latest release version: $VERSION_RELEASE" > $NORMAL_OUT
+echo "Latest release version: $VERSION_RELEASE" >&1
 if [ -e $RELEASE_JAR ]
 then
-  echo "Latest release already downloaded" > $NORMAL_OUT
+  echo "Latest release already downloaded" >&1
 else
   RELEASE_MANIFEST=$(echo $VERSION_MANIFEST_DATA | jq -r --arg VERSION_TARGET $VERSION_RELEASE '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
-  echo "Downloading Package Manifest from: $RELEASE_MANIFEST" > $NORMAL_OUT
+  echo "Downloading Package Manifest from: $RELEASE_MANIFEST" >&1
   RELEASE_MANIFEST_DATA=$(curl -s "$RELEASE_MANIFEST")
-  echo "Done" > $NORMAL_OUT
-  echo "Parsing Package Manifest" > $NORMAL_OUT
+  echo "Done" >&1
+  echo "Parsing Package Manifest" >&1
   RELEASE_NEWJAR_URL=$(jq -rn --argjson url "$RELEASE_MANIFEST_DATA" '$url.downloads.server.url')
   RELEASE_NEWJAR_SHA1=$(jq -rn --argjson sha1 "$RELEASE_MANIFEST_DATA" '$sha1.downloads.server.sha1')
-  echo "Done" > $NORMAL_OUT
-  echo "Downloading release jar" > $NORMAL_OUT
-  curl -s -L -f -o $RELEASE_JAR $RELEASE_NEWJAR_URL
-  echo "Done" > $NORMAL_OUT
-  echo "Calculating SHA1 sum" > $NORMAL_OUT
+  echo "Done" >&1
+  echo "Downloading release jar" >&1
+  curl -L -f -o $RELEASE_JAR $RELEASE_NEWJAR_URL
+  echo "Done" >&1
+  echo "Calculating SHA1 sum" >&1
   RELEASE_SHA1=$(sha1sum $RELEASE_JAR | cut -d " " -f 1 )
   if [ $RELEASE_NEWJAR_SHA1 = $RELEASE_SHA1 ]
   then
-    echo "Hashes match" > $NORMAL_OUT
+    echo "Hashes match" >&1
   else
-    echo "Hashes mismatch - check jar file" > $ERROR_OUT
+    echo "Hashes mismatch - check jar file" >&2
   fi
 fi
 
-echo "Latest snapshot version: $VERSION_SNAPSHOT" > $NORMAL_OUT
+echo "Latest snapshot version: $VERSION_SNAPSHOT" >&1
 if [ -e $SNAPSHOT_JAR ]
 then
-  echo "Latest snapshot already downloaded" > $NORMAL_OUT
+  echo "Latest snapshot already downloaded" >&1
 else
   SNAPSHOT_MANIFEST=$(echo $VERSION_MANIFEST_DATA | jq -r --arg VERSION_TARGET $VERSION_SNAPSHOT '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
-  echo "Downloading Package Manifest from: $SNAPSHOT_MANIFEST" > $NORMAL_OUT
+  echo "Downloading Package Manifest from: $SNAPSHOT_MANIFEST" >&1
   SNAPSHOT_MANIFEST_DATA=$(curl -s "$SNAPSHOT_MANIFEST")
-  echo "Done" > $NORMAL_OUT
-  echo "Parsing Package Manifest" > $NORMAL_OUT
+  echo "Done" >&1
+  echo "Parsing Package Manifest" >&1
   SNAPSHOT_NEWJAR_URL=$(jq -rn --argjson url "$SNAPSHOT_MANIFEST_DATA" '$url.downloads.server.url')
   SNAPSHOT_NEWJAR_SHA1=$(jq -rn --argjson sha1 "$SNAPSHOT_MANIFEST_DATA" '$sha1.downloads.server.sha1')
-  echo "Done" > $NORMAL_OUT
-  echo "Downloading snapshot jar" > $NORMAL_OUT
-  curl -s -L -f -o $SNAPSHOT_JAR $SNAPSHOT_NEWJAR_URL
-  echo "Done" > $NORMAL_OUT
-  echo "Calculating SHA1 sum" > $NORMAL_OUT
+  echo "Done" >&1
+  echo "Downloading snapshot jar" >&1
+  curl -L -f -o $SNAPSHOT_JAR $SNAPSHOT_NEWJAR_URL
+  echo "Done" >&1
+  echo "Calculating SHA1 sum" >&1
   SNAPSHOT_SHA1=$(sha1sum $SNAPSHOT_JAR | cut -d " " -f 1 )
   if [ $SNAPSHOT_NEWJAR_SHA1 = $SNAPSHOT_SHA1 ]
   then
-    echo "Hashes match" > $NORMAL_OUT
+    echo "Hashes match" >&1
   else
-    echo "Hashes mismatch - check jar file" > $ERROR_OUT
+    echo "Hashes mismatch - check jar file" >&2
   fi
 fi
