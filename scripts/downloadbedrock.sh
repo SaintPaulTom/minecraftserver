558f4134faec1634ef5f5614a4365f2c9cfd9879
SaintPaulTom <151305081+SaintPaulTom@users.noreply.github.com>
2024-10-22 20:14:17 -0500

    move update of  preview.version
    update of preview.version if = release.version would never be triggered

--- scripts/downloadbedrock.sh
+++ scripts/downloadbedrock.sh
@@ -32,13 +32,16 @@ PREVIEW_VERSION=${PREVIEW_VERSION%%.zip}
 echo "Latest preview version: "$PREVIEW_VERSION
 if [ -e $PREVIEW_FILE ]; then
         echo "Latest preview already downloaded" >&1
+        # Update preview.version if needed
+  	if cmp --silent -- /opt/minecraft/bedrock/release.version /opt/minecraft/bedrock/preview.version; then
+  		echo "snapshot.version already updataed" >&1
+    	else
+     		echo $VERSION_RELEASE > /opt/minecraft/bedrock/preview.version
+       		echo "snapshot.version updated" >&1
+	fi
 else
         echo "Downloading preview release" >&1
         curl -R -L -f -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" -o  $PREVIEW_FILE $PREVIEW_URL
         echo "Done" >&1
         echo ${PREVIEW_URL##*/} > /opt/minecraft/bedrock/preview.version
-        # Check if preview is the same as release
-        if [ $PREVIEW_VERSION = $RELEASE_VERSION ]; then
-                echo ${RELEASE_URL##*/} > /opt/minecraft/bedrock/preview.version
-        fi
 fi
