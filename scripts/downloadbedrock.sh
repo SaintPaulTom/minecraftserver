db8d3a2418799e0b9963c7091ac9896693aeb9e5
SaintPaulTom <151305081+SaintPaulTom@users.noreply.github.com>
2024-10-22 20:32:29 -0500

    change variables

--- scripts/downloadbedrock.sh
+++ scripts/downloadbedrock.sh
@@ -13,7 +13,7 @@ echo "Done" >&1
 RELEASE_URL=$(echo $BEDROCK_DOWNLOAD_URL_DATA | grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*')
 RELEASE_FILE=$ZIP_PATH/${RELEASE_URL##*/}
 RELEASE_VERSION=${RELEASE_FILE##*r-}
-RELEASE_VERSION=${RELEASE_VERSION%%.zip}
+RELEASE_ZIP=${RELEASE_VERSION%%.zip}
 echo "Latest release version: "$RELEASE_VERSION
 if [ -e $RELEASE_FILE ]; then
         echo "Latest release already downloaded" >&1
@@ -21,14 +21,14 @@ else
         echo "Downloading release zip" >&1
         curl -R -L -f -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" -o $RELEASE_FILE $RELEASE_URL
         echo "Done" >&1
-        echo ${RELEASE_URL##*/} > /opt/minecraft/bedrock/release.version
+        echo $RELEASE_VERSION > /opt/minecraft/bedrock/release.version
 fi
 
 # Extract the preview URL and download the preview release ZIP if not already present
 PREVIEW_URL=$(echo $BEDROCK_DOWNLOAD_URL_DATA | grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux-preview/[^"]*')
 PREVIEW_FILE=$ZIP_PATH/${PREVIEW_URL##*/}
 PREVIEW_VERSION=${PREVIEW_FILE##*r-}
-PREVIEW_VERSION=${PREVIEW_VERSION%%.zip}
+PREVIEW_ZIP=${PREVIEW_VERSION%%.zip}
 echo "Latest preview version: "$PREVIEW_VERSION
 if [ -e $PREVIEW_FILE ]; then
         echo "Latest preview already downloaded" >&1
@@ -36,12 +36,12 @@ if [ -e $PREVIEW_FILE ]; then
   	if cmp --silent -- /opt/minecraft/bedrock/release.version /opt/minecraft/bedrock/preview.version; then
   		echo "snapshot.version already updataed" >&1
     	else
-     		echo $VERSION_RELEASE > /opt/minecraft/bedrock/preview.version
+     		echo $PREVIEW_VERSION > /opt/minecraft/bedrock/preview.version
        		echo "snapshot.version updated" >&1
 	fi
 else
         echo "Downloading preview release" >&1
         curl -R -L -f -H "Accept-Encoding: identity" -H "Accept-Language: en" -s -L -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; BEDROCK-UPDATER)" -o  $PREVIEW_FILE $PREVIEW_URL
         echo "Done" >&1
-        echo ${PREVIEW_URL##*/} > /opt/minecraft/bedrock/preview.version
+        echo $PREVIEW_VERSION > /opt/minecraft/bedrock/preview.version
 fi
