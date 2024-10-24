904e22344ff4bfd6c85c0f87e8f729ebff7409f1
SaintPaulTom <151305081+SaintPaulTom@users.noreply.github.com>
2024-10-23 19:52:52 -0500

    Rewrite downloadjava.sh
    move checking of snapshot = release to end and cleanup

--- scripts/downloadjava.sh
+++ scripts/downloadjava.sh
@@ -4,11 +4,6 @@
 VERSION_MANIFEST='https://piston-meta.mojang.com/mc/game/version_manifest.json'
 JAR_PATH=/opt/minecraft/java/jars
 
-# Check if the jars directory dosen't exist, create it
-if ! [ -e /opt/minecraft/java/jars ]; then
-	mkdir -p /opt/minecraft/java/jars
-fi
-
 #d Download the version manifest
 echo "Downloading Version Manifest from: $VERSION_MANIFEST" >&1
 VERSION_MANIFEST_DATA=$(curl -s "$VERSION_MANIFEST")
@@ -25,64 +20,66 @@ echo "Done" >&1
 # Check and download the latest release version
 echo "Latest release version: $VERSION_RELEASE" >&1
 if [ -e $RELEASE_JAR ]; then
-	echo "Latest release already downloaded" >&1
+        echo "Latest release already downloaded" >&1
 else
-	# Download and parse the release manifest and download the JAR file
-	RELEASE_MANIFEST=$(echo $VERSION_MANIFEST_DATA | jq -r --arg VERSION_TARGET $VERSION_RELEASE '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
-	echo "Downloading Package Manifest from: $RELEASE_MANIFEST" >&1
-	RELEASE_MANIFEST_DATA=$(curl -s "$RELEASE_MANIFEST")
-	echo "Done" >&1
-	echo "Parsing Package Manifest" >&1
-	RELEASE_NEWJAR_URL=$(jq -rn --argjson url "$RELEASE_MANIFEST_DATA" '$url.downloads.server.url')
-	RELEASE_NEWJAR_SHA1=$(jq -rn --argjson sha1 "$RELEASE_MANIFEST_DATA" '$sha1.downloads.server.sha1')
-	echo "Done" >&1
-	echo "Downloading release jar" >&1
-	curl -R -L -f -o $RELEASE_JAR $RELEASE_NEWJAR_URL
-	echo "Done" >&1
- 	# Verify the SHA1 sum
-	echo "Calculating SHA1 sum" >&1
-	RELEASE_SHA1=$(sha1sum $RELEASE_JAR | cut -d " " -f 1 )
-	if [ $RELEASE_NEWJAR_SHA1 = $RELEASE_SHA1 ]; then
-		echo "Hashes match" >&1
-		echo $VERSION_RELEASE > /opt/minecraft/java/release.version
-	else
-		echo "Hashes mismatch - check jar file" >&2
-	fi
+        # Download and parse the release manifest and download the JAR file
+        RELEASE_MANIFEST=$(echo $VERSION_MANIFEST_DATA | jq -r --arg VERSION_TARGET $VERSION_RELEASE '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
+        echo "Downloading Package Manifest from: $RELEASE_MANIFEST" >&1
+        RELEASE_MANIFEST_DATA=$(curl -s "$RELEASE_MANIFEST")
+        echo "Done" >&1
+        echo "Parsing Package Manifest" >&1
+        RELEASE_NEWJAR_URL=$(jq -rn --argjson url "$RELEASE_MANIFEST_DATA" '$url.downloads.server.url')
+        RELEASE_NEWJAR_SHA1=$(jq -rn --argjson sha1 "$RELEASE_MANIFEST_DATA" '$sha1.downloads.server.sha1')
+        echo "Done" >&1
+        echo "Downloading release jar" >&1
+        curl -R -L -f -o $RELEASE_JAR $RELEASE_NEWJAR_URL
+        echo "Done" >&1
+        # Verify the SHA1 sum
+        echo "Calculating SHA1 sum" >&1
+        RELEASE_SHA1=$(sha1sum $RELEASE_JAR | cut -d " " -f 1 )
+        if [ $RELEASE_NEWJAR_SHA1 = $RELEASE_SHA1 ]; then
+                echo "Hashes match" >&1
+                echo $VERSION_RELEASE > /opt/minecraft/java/release.version
+        else
+                echo "Hashes mismatch - check jar file" >&2
+        fi
 fi
 
 # Check and download the latest snapshot version
 echo "Latest snapshot version: $VERSION_SNAPSHOT" >&1
 if [ -e $SNAPSHOT_JAR ]; then
-	echo "Latest snapshot already downloaded" >&1
-	# Update snapshot.version if needed
-  	if cmp --silent -- /opt/minecraft/java/release.version /opt/minecraft/java/snapshot.version; then
-  		echo "snapshot.version already updataed" >&1
-    	else
-     		echo $VERSION_RELEASE > /opt/minecraft/java/snapshot.version
-       		echo "snapshot.version updated" >&1
-	fi
+        echo "Latest snapshot already downloaded" >&1
 else
-	# Download and parse the snapshot manifest and download the JAR file
-	SNAPSHOT_MANIFEST=$(echo $VERSION_MANIFEST_DATA | jq -r --arg VERSION_TARGET $VERSION_SNAPSHOT '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
-	echo "Downloading Package Manifest from: $SNAPSHOT_MANIFEST" >&1
-	SNAPSHOT_MANIFEST_DATA=$(curl -s "$SNAPSHOT_MANIFEST")
-	echo "Done" >&1
-	echo "Parsing Package Manifest" >&1
-	SNAPSHOT_NEWJAR_URL=$(jq -rn --argjson url "$SNAPSHOT_MANIFEST_DATA" '$url.downloads.server.url')
-	SNAPSHOT_NEWJAR_SHA1=$(jq -rn --argjson sha1 "$SNAPSHOT_MANIFEST_DATA" '$sha1.downloads.server.sha1')
-	echo "Done" >&1
-	echo "Downloading snapshot jar" >&1
-	curl -R -L -f -o $SNAPSHOT_JAR $SNAPSHOT_NEWJAR_URL
-	echo "Done" >&1
- 	# Verify the SHA1 sum
-	echo "Calculating SHA1 sum" >&1
-	SNAPSHOT_SHA1=$(sha1sum $SNAPSHOT_JAR | cut -d " " -f 1 )
-	if [ $SNAPSHOT_NEWJAR_SHA1 = $SNAPSHOT_SHA1 ]; then
-		echo "Hashes match" >&1
-		# Update snapshot version if same as release
-	else
-		echo "Hashes mismatch - check jar file" >&2
-	fi
+        # Download and parse the snapshot manifest and download the JAR file
+        SNAPSHOT_MANIFEST=$(echo $VERSION_MANIFEST_DATA | jq -r --arg VERSION_TARGET $VERSION_SNAPSHOT '.versions | .[] | select(.id==$VERSION_TARGET) | .url')
+        echo "Downloading Package Manifest from: $SNAPSHOT_MANIFEST" >&1
+        SNAPSHOT_MANIFEST_DATA=$(curl -s "$SNAPSHOT_MANIFEST")
+        echo "Done" >&1
+        echo "Parsing Package Manifest" >&1
+        SNAPSHOT_NEWJAR_URL=$(jq -rn --argjson url "$SNAPSHOT_MANIFEST_DATA" '$url.downloads.server.url')
+        SNAPSHOT_NEWJAR_SHA1=$(jq -rn --argjson sha1 "$SNAPSHOT_MANIFEST_DATA" '$sha1.downloads.server.sha1')
+        echo "Done" >&1
+        echo "Downloading snapshot jar" >&1
+        curl -R -L -f -o $SNAPSHOT_JAR $SNAPSHOT_NEWJAR_URL
+        echo "Done" >&1
+        # Verify the SHA1 sum
+        echo "Calculating SHA1 sum" >&1
+        SNAPSHOT_SHA1=$(sha1sum $SNAPSHOT_JAR | cut -d " " -f 1 )
+        if [ $SNAPSHOT_NEWJAR_SHA1 = $SNAPSHOT_SHA1 ]; then
+                echo "Hashes match" >&1
+                echo $VERSION_SNAPSHOT > /opt/minecraft/java/snapshot.version
+
+        else
+                echo "Hashes mismatch - check jar file" >&2
+        fi
 fi
 
-
+# Update snapshot version if same as release
+if [ $VERSION_RELEASE = $VERSION_SNAPSHOT ]; then
+        if cmp --silent -- /opt/minecraft/java/release.version /opt/minecraft/java/snapshot.version; then
+                echo "snapshot.version already updated" >&1
+        else
+                echo $VERSION_RELEASE > /opt/minecraft/java/snapshot.version
+                echo "snapshot.version updataed" >&1
+        fi
+fi
